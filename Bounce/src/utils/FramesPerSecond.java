83d8aada8e24436e64caad76aae721a40f93a9ec
Fonserbc <Fonserbc@gmail.com>
2012-09-18 10:39:33 +0200

    Trying to resolve merging conflicts

--- Bounce/src/utils/FramesPerSecond.java
+++ Bounce/src/utils/FramesPerSecond.java
@@ -3,6 +3,8 @@ package utils;
 import java.text.DecimalFormat;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 
 public class FramesPerSecond {
 	
@@ -10,48 +12,80 @@ public class FramesPerSecond {
 	
 	private DecimalFormat df = new DecimalFormat("0.##");
 	
-	private final static int 	STAT_INTERVAL_FPS = 300;
+	private final static double 	STAT_INTERVAL_FPS = 0.1;
 	
 	private final static int	FPS_HISTORY = 10;
 	
 	private double 	tickStore[];
 	private double 	averageFps = 0.0;
 	
+	private double intervalTime;
+	
 	private Timer timerFPS;
+	private Timer intervalTimer;
 	
 	private String fpsString = "FPS:";
 	
-	private int it = 0;
+	private int it = 0;	
 	
-	
-	private long WANNABE_TICK = 1000 / FPS;
+	private long WANNABE_TICK;
 	
 	private long lastTickStart = 0;
 	
-	public FramesPerSecond() {
+	private long lastTickEnd = 0;
+	
+	private long recommendedSleepTime = 0;
+	
+	public FramesPerSecond(int fps) {
 		timerFPS = new Timer();
+		intervalTimer = new Timer();
 		
 		tickStore = new double[FPS_HISTORY];
 		for (int i = 0; i < FPS_HISTORY; i++)
 			tickStore[i] = 0.0;
+		
+		FPS = fps;
+		WANNABE_TICK = 1000 / FPS;
 	}
 	
 	public void tickStart() {
-		timerFPS.tick();
+		tickStore[it] = timerFPS.tick();
+		
+		it = (it + 1)%FPS_HISTORY;
 		
 		lastTickStart = timerFPS.getLastTickMs();
 	}
 	
 	public void tickEnd() {
+		lastTickEnd = timerFPS.falseTickMs();
 		
+		recommendedSleepTime = 2*WANNABE_TICK - lastTickStart - lastTickEnd;
 	}
 	
 	public long getSleepTime() {
 		
-		return 0;
+		return recommendedSleepTime;
 	}
 	
 	public void doDraw(Canvas c) {
+		intervalTime += intervalTimer.tick();
 		
+		if (intervalTime > STAT_INTERVAL_FPS) {
+			intervalTime = 0.0;
+			
+			averageFps = 0.0;
+			for (int i = 0; i < FPS_HISTORY; ++i)
+				averageFps += tickStore[i];
+			
+			averageFps /= FPS_HISTORY;
+			averageFps = 1/averageFps;
+			
+			fpsString = "FPS: " + df.format(averageFps);
+		}
+		
+		Paint paint = new Paint();
+		paint.setColor(Color.BLACK);
+		paint.setTextSize(20);
+		c.drawText(fpsString+" / "+FPS, 20, 20, paint);
 	}
 }
